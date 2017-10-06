8467c4baf0ac5f9a5f8ee3c7968f82b8eea1973b
david226 <30418773+david226@users.noreply.github.com>
2017-10-06 15:13:44 -0500

    agregue peronaJuridica

---
+++ TrabajoMvc/TrabajoMvc/Controllers/PersonaJuridicaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrabajoMvc.Models;
+
+namespace TrabajoMvc.Controllers
+{
+    public class PersonaJuridicaController : Controller
+    {
+        // GET: PersonaJuridica
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Insertar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insertar4(PersonaJuridica personaJuridica)
+        {
+
+            return View();
+        }
+    }
+}
