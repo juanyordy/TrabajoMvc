552caad30db15ab5626e6e1f0883c32c61220393
juanyordyguillen <juanyordyguillen@gmail.com>
2017-10-06 16:09:40 -0500

    creando los VMmodel

---
+++ TrabajoMvc/TrabajoMvc/Vmodel/VmPersonaJuridica.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using TrabajoMvc.Models;
+
+namespace TrabajoMvc.Vmodel
+{
+    public class VmPersonaJuridica
+    {
+        public PersonaJuridica personaJuridica { get; set; }
+        public List<PersonaJuridica> listaPersonaJuridica { get; set; } 
+    }
+}
---
+++ TrabajoMvc/TrabajoMvc/Vmodel/VmPersonaNAtural.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrabajoMvc.Models;
+
+namespace TrabajoMvc.Vmodel
+{
+    public class VmPersonaNatural
+    {
+        public PersonaNatural personaJuridica { get; set; }
+        public List<PersonaNatural> listaPersonaJuridica { get; set; }
+    }
+}
