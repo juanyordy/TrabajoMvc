c9ecc2becae1aa18facf5ccf1fb4c823b0607f45
juanyordyguillen <juanyordyguillen@gmail.com>
2017-10-06 14:40:40 -0500

    implementando metodoe em Query persona

--- TrabajoMvc/TrabajoMvc/Query/QPersonaJuridica.cs
+++ TrabajoMvc/TrabajoMvc/Query/QPersonaJuridica.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TrabajoMvc.Interface;
+using TrabajoMvc.Models;
 
 namespace TrabajoMvc.Query
 {
-    public class QPersonaJuridica
+    public class QPersonaJuridica : IGeneric<Persona>
     {
+        public bool Editar(Persona t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(Persona t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool insertar(Persona t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Persona> Listar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
