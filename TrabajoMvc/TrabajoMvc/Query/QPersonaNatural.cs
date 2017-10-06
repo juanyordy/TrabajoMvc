9cd16820a9470713bcff9f018685e77d78dedb2a
juanyordyguillen <juanyordyguillen@gmail.com>
2017-10-06 14:49:09 -0500

    implentando en el query persona natural

--- TrabajoMvc/TrabajoMvc/Query/QPersonaNatural.cs
+++ TrabajoMvc/TrabajoMvc/Query/QPersonaNatural.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TrabajoMvc.Interface;
+using TrabajoMvc.Models;
 
 namespace TrabajoMvc.Query
 {
-    public class QPersonaNatural
+    public class QPersonaNatural : IGeneric<PersonaNatural>
     {
+        public bool Editar(PersonaNatural t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(PersonaNatural t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool insertar(PersonaNatural t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PersonaNatural> Listar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
